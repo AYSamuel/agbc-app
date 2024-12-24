73a87db9495fce04bf1df8f9ee52a953e7ab887e
AYSamuel <aysamuel007@gmail.com>
2024-12-24 04:58:56 +0100

    Add Firebase and dotenv integration to Flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  // Load environment variables before initializing Firebase
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase using environment variables
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
